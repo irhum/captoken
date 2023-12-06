438e88e89bd2f32d1294006307698305044a9b7f
Irhum Shafkat <13271155+irhum@users.noreply.github.com>
2023-12-06 15:48:30 -0300

    Upstream changes for full draft

--- README.md
+++ README.md
@@ -2,4 +2,11 @@ Tokenizers will tokenize capitalized versions of words differently than lowercas
 
 At its core, a language model is simply a model of humans pressing a keyboard. What if we took that literally, and "placed in" the SHIFT and Caps Lock button presses needed to produce text? That way, capitalization could be represented *seperately* from the words themselves.
 
-We do this with four specialized tokens \<SHIFT>, \<CAPSS>, \<CAPSE> and \<bksp> (which are always tokenized as whole, never broken) to absorb this syntactic information, leaving all the other words lowercased.
+We do this with four specialized tokens \<shift>, \<capss>, \<capse> and \<bksp> (which are always tokenized as whole, never broken) to absorb this syntactic information, leaving all the other words lowercased.
+
+### Current Status
+There are four self contained notebooks in this repository, inside `notebooks`:
+
+1. `GPT2 Tokenizer.ipynb` - contains baseline experiments on the GPT-2 tokenizer, to quantify the number of redundant tokens.
+2. `Train Tokenizer` - This trains the two 16k vocabulary SentencePiece tokenizers on the Wikipedia dataset.
+3. `Newsgroups.ipynb` - This (and the `Caps` variant) performs the experiments and plots using the two trained tokenizers.
--- captoken/preprocess.py
+++ captoken/preprocess.py
@@ -1,17 +1,25 @@
 import regex as re
 
-def replace_caps_fn(matchv):
-    caps_text = matchv.group(0).lower()
-    if len(caps_text.strip()) > 1:
-        text_len = len(caps_text.rstrip())
-        return f"<CAPSS>{caps_text[:text_len]}<CAPSE>{caps_text[text_len:]}"
-    else:
-        return f"<SHIFT>{caps_text}"
+# Replacement function for re.sub, with the <capss> and <capse> tags.
+def replace_caps_fn(m):
+    # Concatenate <capss> and <capse> tags with  lowercase version of matched group.
+    return '<capss>' + m.group(1).lower() + '<capse>' + m.group(3)
 
+# Main function to replace uppercase words, and insert tags.
 def replace_caps(text):
-    if text[0] != ' ': text = ' ' + text
-    text = re.sub(r'([^\x20\w])([^\s\p{P}])', r'\1<bksp> \2', text)
-    pattern = u'\x20*(\p{Lu}+[\s\p{P}\p{Lu}]*)+'
+    # Add a space at the beginning of the text, if it doesn't start with a space.
+    if text[0] != ' ':
+        text = ' ' + text
+    
+    # Insert a '<bksp> ' for all words that are immediately preceded by punctuation.
+    text = re.sub(r'([^\x20\w])([^\s\p{P}\p{Nd}])', r'\1<bksp> \2', text)
 
-    result = re.sub(pattern, replace_caps_fn, text)
-    return result
+    # Regex pattern matching all spans on ALL CAPS text.
+    pattern = u'(\x20*\p{Lu}{2,}([\s\p{P}]*\p{Lu}{2,})*)([\s\p{P}])'
+    # Replace all caps span with tags + lowercase
+    text = re.sub(pattern, replace_caps_fn, text)
+
+    # Replace all caps letters with <shift> + lowered version.
+    text = re.sub(u'(\x20+[\p{Lu}])', lambda x: '<shift>' + x.group(1).lower(), text)
+    
+    return text
